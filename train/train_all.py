866513aee37f33b0a462fd5754828d7ca2a47fc0
Patvath88 <patvath8@gmail.com>
2025-11-21 10:53:00 -0500

    Update train_all.py

--- train/train_all.py
+++ train/train_all.py
@@ -1,6 +1,16 @@
 from train_model import train_stat
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv("data/model_dataset.csv")
+
+    train_stat(df, "points", "points")
+    train_stat(df, "rebounds", "rebounds")
+    train_stat(df, "assists", "assists")
+
+    print("All models trained successfully.")
+
 
 if __name__ == "__main__":
-    train_stat("points")
-    train_stat("rebounds")
-    train_stat("assists")
+    main()
